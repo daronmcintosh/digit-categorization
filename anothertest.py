f92b278919088ee4d29f5e56c46a44e87780c347
Daron McIntosh <mcintoshdaron@gmail.com>
2019-04-28 15:16:44 -0400

    remove test files

--- anothertest.py
+++
@@ -1,8 +0,0 @@
-from sklearn import datasets
-from sklearn.multiclass import OneVsOneClassifier
-from sklearn.svm import LinearSVC
-
-iris = datasets.load_iris()
-X, y = iris.data, iris.target
-score = OneVsOneClassifier(LinearSVC(random_state=0)).fit(X, y).score(X, y)
-print(score)
